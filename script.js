fbef90ce601e2b1ce21b6affea877fd77e908f6c
daniel <daniplingau@gmail.com>
2024-06-06 10:52:55 +0300

    The initial functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(min, sub) {
+  return min - sub;
+}
+
+function product(a, b) {
+  return a * b;
+}
+
+function division(dividend, divisor) {
+  return dividend / divisor;
+}
